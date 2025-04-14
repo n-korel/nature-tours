fc104b526f892f03a614b2c8c8eed2b780bc4c52
n-korel <n.korelskiy@hotmail.com>
2025-04-15 01:06:13 +0300

    middleware fix

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const slugify = require('slugify');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { default: slugify } = require('slugify');
 
 const tourSchema = new mongoose.Schema(
 	{
@@ -9,6 +10,7 @@ const tourSchema = new mongoose.Schema(
 			unique: true,
 			trim: true,
 		},
+		slug: String,
 		duration: {
 			type: Number,
 			required: [true, 'A toure must have a duration'],
@@ -54,6 +56,10 @@ const tourSchema = new mongoose.Schema(
 			select: false,
 		},
 		startDates: [Date],
+		secretTour: {
+			type: Boolean,
+			default: false,
+		},
 	},
 	{
 		toJSON: { virtuals: true },
@@ -65,8 +71,22 @@ tourSchema.virtual('durationWeeks').get(function () {
 	return this.duration / 7;
 });
 
-tourSchema.pre('save', function () {
-	console.log(this);
+//document middleware: runs before .save() or .create()
+tourSchema.pre('save', function (next) {
+	this.slug = slugify(this.name, { lower: true });
+	next();
+});
+
+//query middleware
+tourSchema.pre(/^find/, function (next) {
+	this.find({ secretTour: { $ne: true } });
+	next();
+});
+
+//aggregation middleware
+tourSchema.pre('aggregate', function (next) {
+	this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+	next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
