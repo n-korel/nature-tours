eeb4421f2595f89382069d2535d13115a60f56bf
n-korel <n.korelskiy@hotmail.com>
2025-04-15 17:13:07 +0300

    Catching Errors

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-// eslint-disable-next-line import/no-extraneous-dependencies
 const { default: slugify } = require('slugify');
 
 const tourSchema = new mongoose.Schema(
@@ -9,6 +8,8 @@ const tourSchema = new mongoose.Schema(
 			required: [true, 'A tour must have a name'],
 			unique: true,
 			trim: true,
+			maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+			minlength: [10, 'A tour name must have more or equal then 10 characters'],
 		},
 		slug: String,
 		duration: {
@@ -22,10 +23,16 @@ const tourSchema = new mongoose.Schema(
 		difficulty: {
 			type: String,
 			required: [true, 'A toure must have a difficulty'],
+			enum: {
+				values: ['easy', 'medium', 'difficult'],
+				message: 'Difficulty is either: easy, medium, difficult',
+			},
 		},
 		ratingsAverage: {
 			type: Number,
 			default: 4.5,
+			min: [1, 'Rating must be above 1.0'],
+			max: [5, 'Rating must be below 5.0'],
 		},
 		ratingsQuantity: {
 			type: Number,
@@ -35,7 +42,16 @@ const tourSchema = new mongoose.Schema(
 			type: Number,
 			required: [true, 'A tour must have a price'],
 		},
-		priceDiscount: Number,
+		priceDiscount: {
+			type: Number,
+			validate: {
+				validator: function (val) {
+					//only for new creation document
+					return val < this.price;
+				},
+				message: 'Discount price ({VALUE}) should be below regular price',
+			},
+		},
 		summary: {
 			type: String,
 			trim: true,
