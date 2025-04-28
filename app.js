07d90b6c1a60d4d15a1f233ac15e868ab84b5db2
n-korel <n.korelskiy@hotmail.com>
2025-04-28 18:13:00 +0300

    Base Template

--- app.js
+++ app.js
@@ -20,7 +20,15 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // GLOBAL MIDDLEWARES
+
+// Serving static files
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Set security HTTP headers
 app.use(helmet());
 
@@ -60,16 +68,20 @@ app.use(
 	}),
 );
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // Test middleware
 app.use((req, res, next) => {
 	req.requestTime = new Date().toISOString();
 	next();
 });
 
-// Routes
+// ROUTES
+app.get('/', (req, res) => {
+	res.status(200).render('base', {
+		tour: 'the Forest Hiker',
+		user: 'Jonas',
+	});
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
