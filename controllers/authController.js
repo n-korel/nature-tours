9bfa20578848249613fa01cbcb7782a523dc8e69
n-korel <n.korelskiy@hotmail.com>
2025-04-28 15:10:44 +0300

    API completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -99,7 +99,7 @@ export const protect = catchAsync(async (req, res, next) => {
 
 export const restrictTo = (...roles) => {
 	return (req, res, next) => {
-		// roles ['admin, 'lead-guide']
+		// roles ['admin, 'lead-guide', 'user', 'guide']
 		if (!roles.includes(req.user.role)) {
 			return next(new AppError('You do not have permission to perform this action', 403));
 		}
